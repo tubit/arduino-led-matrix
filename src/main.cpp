7a0616eb225a7ece9da3a64737cf0413e73642cb
Dennis Konert <dkonert@gmail.com>
2023-03-13 18:35:54 +0100

    Strings instead of charArrays

--- src/main.cpp
+++ src/main.cpp
@@ -21,8 +21,8 @@ void handleConfigure();
 void sendHeader();
 void sendFooter();
 
-void printText(uint8_t modStart, uint8_t modEnd, char *pMsg);
 void scrollText(String text);
+void printText(String text);
 /* end declare functions */
 
 ESP8266WebServer server(WEBSERVER_PORT);
@@ -30,8 +30,7 @@ ESP8266HTTPUpdateServer serverUpdater;
 int8_t hold_seconds = 0;
 
 // for message scrolling
-char message[BUF_SIZE] = "Hello World";
-String new_message = "Hello World";
+String message = "Hello World";
 bool newMessageAvailable = true;
 bool loop_message = true;
 
@@ -66,11 +65,10 @@ void setup() {
   pinMode(buttonWifiReset, INPUT_PULLDOWN_16);
 
   mx.begin();
+  mx.clear();
   Serial.println("Matrix created");
   mx.control(MD_MAX72XX::INTENSITY, 0);
-  String moin = "moin!";
-  moin.toCharArray(message, BUF_SIZE);
-  printText(0, MAX_DEVICES-1, message);
+  printText("moin!");
 
   for (int inx = 0; inx <= 15; inx++) {
     mx.control(MD_MAX72XX::INTENSITY, inx);
@@ -141,7 +139,7 @@ void setup() {
   }
 
   // Scroll your own hostname after first boot.
-  new_message = WiFi.getHostname();
+  message = WiFi.getHostname();
   newMessageAvailable = true;
 
   // set timezone
@@ -174,9 +172,9 @@ void loop() {
 //  }
 
   if (newMessageAvailable) {
-    Serial.println("Processing message: " + String(new_message));
+    Serial.println("Processing message: " + String(message));
     mx.control(MD_MAX72XX::INTENSITY, 2);
-    scrollText(new_message);
+    scrollText(message);
     newMessageAvailable = false;
     update_matrix = true;
   }
@@ -214,9 +212,8 @@ void loop() {
     Serial.print("Update clock: ");
     Serial.println(current_time);
 
-    current_time.toCharArray(message, BUF_SIZE);
     mx.control(MD_MAX72XX::INTENSITY, 0);
-    printText(0, MAX_DEVICES-1, message);
+    printText(current_time);
   }
   delay(10);
 }
@@ -264,62 +261,20 @@ void scrollText(String text) {
   }
 }
 
-// Print the text string to the LED matrix modules specified.
-// Message area is padded with blank columns after printing.
-void printText(uint8_t modStart, uint8_t modEnd, char *pMsg) {
-  uint8_t   state = 0;
-  uint8_t   curLen = 0;
-  uint16_t  showLen = 0;
-  uint8_t   cBuf[8];
-  int16_t   col = ((modEnd + 1) * COL_SIZE) - 1;
+void printText(String text) {
+  int16_t col = (MAX_DEVICES * COL_SIZE) - 1;
+  int8_t charWidth = 8;
+  uint8_t cBuf[8];  // this should be ok for all built-in fonts
 
-  mx.control(modStart, modEnd, MD_MAX72XX::UPDATE, MD_MAX72XX::OFF);
+  mx.clear();
 
-  do     // finite state machine to print the characters in the space available
-  {
-    switch(state) {
-      case 0: // Load the next character from the font table
-        // if we reached end of message, reset the message pointer
-        if (*pMsg == '\0') {
-          showLen = col - (modEnd * COL_SIZE);  // padding characters
-          state = 2;
-          break;
-        }
+  // print each char, but stop if it is not fitting
+  for (unsigned int idx = 0; idx <= text.length() && col >= 0; idx++) {
+    mx.setChar(col, text.charAt(idx));
 
-        // retrieve the next character form the font file
-        showLen = mx.getChar(*pMsg++, sizeof(cBuf)/sizeof(cBuf[0]), cBuf);
-        curLen = 0;
-        state++;
-        // no break !! deliberately fall through to next state to start displaying
-
-      case 1: // display the next part of the character
-        mx.setColumn(col--, cBuf[curLen++]);
-
-        // done with font character, now display the space between chars
-        if (curLen == showLen) {
-          showLen = CHAR_SPACING;
-          state = 2;
-        }
-        break;
-
-      case 2: // initialize state for displaying empty columns
-        curLen = 0;
-        state++;
-        // fall through
-
-      case 3:	// display inter-character spacing or end of message padding (blank columns)
-        mx.setColumn(col--, 0);
-        curLen++;
-        if (curLen == showLen)
-          state = 0;
-        break;
-
-      default:
-        col = -1;   // this definitely ends the do loop
-    }
-  } while (col >= (modStart * COL_SIZE));
-
-  mx.control(modStart, modEnd, MD_MAX72XX::UPDATE, MD_MAX72XX::ON);
+    charWidth = mx.getChar(utf8ascii(text.charAt(idx)), sizeof(cBuf) / sizeof(cBuf[0]), cBuf);
+    col = col - CHAR_SPACING - charWidth;
+  }
 }
 
 void flashLED(int count, int delayTime) {
@@ -356,7 +311,7 @@ void handleRoot() {
   if (server.method() == HTTP_POST) {
     for (uint8_t i = 0; i < server.args(); i++) {
       if (server.argName(i) == "message") {
-        new_message = server.arg(i);
+        message = server.arg(i);
         newMessageAvailable = true;
       } else if (server.argName(i) == "animation") {
         animation = server.arg(i).toInt();
